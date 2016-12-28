0d5abb32fafab5fee12cc6f0f4764023f615f4eb
vishw28 <vishwvajaria@gmail.com>
2016-12-28 02:35:28 -0500

    feat(edit note)

--- public/controllers/notesController.js
+++ public/controllers/notesController.js
@@ -10,6 +10,7 @@
 
     function NotesController (notesService) {
             var nc = this;
+            var save_id;
             nc.form = false;
             nc.notes = {};
             nc.allNotes = notesService.getNotes();
@@ -22,13 +23,21 @@
             nc.addNote = function () {
                 notesService.addNotes(nc.notes);
                 nc.notes = {};
-
-                angular.forEach(nc.allNotes,function (x) {
-                    console.log(x.title);
-                })
             }
-            nc.deleteNote = function (id) {
-                nc.allNotes.splice(id,1);
+            nc.deleteNote = function (delete_id) {
+                nc.allNotes.splice(delete_id,1);
+            }
+            nc.editNote =function (edit_id) {
+                nc.edit = true;
+                nc.notes.title = nc.allNotes[edit_id].title;
+                nc.notes.note = nc.allNotes[edit_id].note;
+                save_id = edit_id;
+            }
+
+            nc.saveNote = function () {
+                nc.allNotes.splice(save_id,1);
+                nc.addNote();
+                nc.edit = false;
             }
 
     }
